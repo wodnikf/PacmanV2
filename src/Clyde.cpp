90d567cf61ce0e33cad699777072fe8164b5487e
Krzysztof Kłos <250252@edu.p.lodz.pl>
2025-01-06 01:43:50 +0100

    Reverted commit

--- src/Clyde.cpp
+++ src/Clyde.cpp
@@ -27,28 +27,11 @@ void Clyde::loadAnimation()
     }
 }
 
-Point generateRandomPos(const Map *map)
+void Clyde::chase(const Map *map, Pathfinder &pathfinder)
 {
     srand(time(nullptr));
     const int X_SIZE = map->getWidth();
     const int Y_SIZE = map->getHeight();
-    Point randomTarget;
-
-    do
-    {
-        const int randomX = rand() % X_SIZE;
-        const int randomY = rand() % Y_SIZE;
-        randomTarget = {randomX * Globals::TILE_SIZE, randomY * Globals::TILE_SIZE};
-    }
-    while (map->getTile(randomTarget).getType() == Wall ||
-           map->getTile(randomTarget).getType() == GhostHouse);
-
-    return randomTarget;
-}
-
-
-void Clyde::chase(const Map *map, Pathfinder &pathfinder)
-{
     static Point targetPos;
     static bool firstTime = true;
 
@@ -56,14 +39,32 @@ void Clyde::chase(const Map *map, Pathfinder &pathfinder)
 
     if (firstTime || targetAchieved)
     {
-        targetPos = generateRandomPos(map);
+        int randomX = rand() % X_SIZE;
+        int randomY = rand() % Y_SIZE;
+        targetPos = {randomX * Globals::TILE_SIZE, randomY * Globals::TILE_SIZE};
+
+        while (map->getTile(targetPos).getType() == Wall || map->getTile(targetPos).getType() == GhostHouse)
+        {
+            randomX = rand() % X_SIZE;
+            randomY = rand() % Y_SIZE;
+            targetPos = {randomX * Globals::TILE_SIZE, randomY * Globals::TILE_SIZE};
+        }
     }
 
     path = pathfinder.findPath(snapToGrid(position), targetPos, false);
 
     while (path.empty())
     {
-        targetPos = generateRandomPos(map);
+        int randomX = rand() % X_SIZE;
+        int randomY = rand() % Y_SIZE;
+        targetPos = {randomX * Globals::TILE_SIZE, randomY * Globals::TILE_SIZE};
+
+        while (map->getTile(targetPos).getType() == Wall || map->getTile(targetPos).getType() == GhostHouse)
+        {
+            randomX = rand() % X_SIZE;
+            randomY = rand() % Y_SIZE;
+            targetPos = {randomX * Globals::TILE_SIZE, randomY * Globals::TILE_SIZE};
+        }
         path = pathfinder.findPath(snapToGrid(position), targetPos, false);
     }
 
